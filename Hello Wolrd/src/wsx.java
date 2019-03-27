8298a236bbe230613e9b644c46831547d5afed3f
bongkee <bongkee9175@gmail.com>
2019-03-27 12:03:10 +0900

    a

--- Hello Wolrd/src/wsx.java
+++ Hello Wolrd/src/wsx.java
@@ -4,7 +4,7 @@ public class wsx {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
 		System.out.println("Earth's End Dancehall");
-		System.out.println("Hello Meke");
+		System.out.println("Hello Me");
 	}
 
 }
